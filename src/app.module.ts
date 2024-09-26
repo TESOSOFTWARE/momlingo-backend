e264b0380b953782fc9bab9f82b112a3cb08abdc
ThaiKV <thaikv@tesoglobal.com>
2024-09-26 18:04:02 +0700

    Doing JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import {ConfigAppModule} from "./config/app/config.app.module";
 import {UsersModule} from './models/users/users.module';
 import {ConfigModule} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 import typeOrmConfig from "./config/database/mysql/typeorm.config";
 
 import * as dotenv from 'dotenv';
@@ -15,8 +18,9 @@ dotenv.config();
         ConfigAppModule,
         ConfigModule.forRoot(),
         TypeOrmModule.forRoot(typeOrmConfig),
-        UsersModule],
-    controllers: [AppController],
-    providers: [AppService],
+        UsersModule,
+        AuthModule],
+    controllers: [AppController, AuthController],
+    providers: [AppService, AuthService],
 })
 export class AppModule {}
--- src/models/users/users.service.ts
+++ src/models/users/users.service.ts
@@ -3,12 +3,34 @@ import {User} from "./entities/user.entity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 
+export type UserLocal = any;
+
+
 @Injectable()
 export class UsersService {
+    private readonly users = [
+        {
+            id: 1,
+            name: 'john',
+            email: 'john@gmail.com',
+            password: 'changeme',
+        },
+        {
+            id: 2,
+            name: 'maria',
+            email: 'maria@gmail.com',
+            password: 'guess',
+        },
+    ];
+
     constructor(
         @InjectRepository(User)
         private usersRepository: Repository<User>,
     ) {}
+
+    async findOneLocal(name: string): Promise<UserLocal | undefined> {
+        return this.users.find(user => user.name === name);
+    }
 
     findAll(): Promise<User[]> {
         return this.usersRepository.find();
