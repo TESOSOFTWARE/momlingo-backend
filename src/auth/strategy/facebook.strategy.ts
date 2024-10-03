27451e27ff6759490c47f9273c4ee1d9f5ab0445
ThaiKV <thaikv@tesoglobal.com>
2024-10-03 17:12:51 +0700

    Done login facebook on web

--- src/auth/strategy/facebook.strategy.ts
+++ src/auth/strategy/facebook.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile } from 'passport-facebook';
 import { AuthService } from '../auth.service';
+import { Buffer } from 'buffer';
 
 @Injectable()
 export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
@@ -10,32 +11,34 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
       callbackURL: 'http://localhost:3000/auth/facebook/callback',
-      // profileFields: ['id', 'displayName', 'emails', 'photos'],
-      scope: ['email, profile'],
-      // profileFields: ["emails", "name"],
-      passReqToCallback: true,
+      // profileFields: ['displayName', 'email', 'profile'],
     });
   }
 
   async validate(
     accessToken: string,
     refreshToken: string,
-    profile: Profile,
+    profile: Profile, // undefined
     done: (err: any, user: any, info?: any) => void,
   ): Promise<any> {
-    console.log('profile');
-    console.log(profile);
-    const { name, emails } = profile;
-    const user = {
-      email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-    };
-    const payload = {
-      user,
-      accessToken,
-    };
+    console.log(`accessToken: ${accessToken}`);
+    const facebookProfile =
+      await this.authService.getFacebookProfile(accessToken);
+    if (facebookProfile) {
+      const name = Buffer.from(facebookProfile.data.name, 'utf-8').toString(
+        'utf-8',
+      );
+      const user = {
+        email: facebookProfile.data.email,
+        name: name,
+        avatarUrl: facebookProfile.data.picture.data.url,
+      };
+      const payload = {
+        user,
+        accessToken,
+      };
 
-    done(null, payload);
+      done(null, payload);
+    }
   }
 }
