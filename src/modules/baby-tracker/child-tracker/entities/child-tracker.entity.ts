a922ead16fdb1ded3ef269c666698ee62d117622
ThaiKV <thaikv95@gmail.com>
2024-10-27 11:10:44 +0700

    Add child-tracker entity and migration

---
+++ src/modules/baby-tracker/child-tracker/entities/child-tracker.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('child-trackers')
+export class BabyInfo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  week: number;
+
+  @Column('text', { nullable: true })
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+}
