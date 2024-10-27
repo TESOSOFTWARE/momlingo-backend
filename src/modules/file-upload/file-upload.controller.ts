73afaeec851b65d02ab42d74dc4fe95ba9039b4b
ThaiKV <thaikv95@gmail.com>
2024-10-27 10:20:37 +0700

    Remove unused code

--- src/modules/file-upload/file-upload.controller.ts
+++ src/modules/file-upload/file-upload.controller.ts
@@ -1,13 +1,9 @@
 import {
   Controller,
-  Post,
-  UseInterceptors,
   UseGuards,
-  UploadedFile,
 } from '@nestjs/common';
-import { FileUploadService, getMulterOptions } from './file-upload.service';
+import { FileUploadService } from './file-upload.service';
 import { JwtGuard } from '../auth/guards/jwt.guard';
-import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('upload')
@@ -15,18 +11,4 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 @UseGuards(JwtGuard)
 export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
-
-  @Post('user-avatar')
-  @UseInterceptors(FileInterceptor('avatar', getMulterOptions('user-avatars')))
-  async uploadUserAvatar(@UploadedFile() file: Express.Multer.File) {
-    // const savedFile = await this.fileUploadService.saveFile(file);
-    return { message: 'Avatar uploaded successfully', file: file.path };
-  }
-
-  @Post('child-avatar')
-  @UseInterceptors(FileInterceptor('avatar', getMulterOptions('child-avatars')))
-  async uploadChildAvatar(@UploadedFile() file: Express.Multer.File) {
-    // const savedFile = await this.fileUploadService.saveFile(file);
-    return { message: 'Avatar uploaded successfully', file: file.path };
-  }
 }
