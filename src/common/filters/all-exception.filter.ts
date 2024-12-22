1781846f8b8456d113723e770ac37ce1a268bdab
ThaiKV <thaikv95@gmail.com>
2024-12-23 00:20:19 +0700

    Fix try catch all

--- src/common/filters/all-exception.filter.ts
+++ src/common/filters/all-exception.filter.ts
@@ -13,14 +13,15 @@ import { FileUploadService } from '../../modules/file-upload/file-upload.service
 export class AllExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(HttpExceptionFilter.name);
 
-  constructor(private readonly fileUploadService: FileUploadService) {}
+  constructor(private readonly fileUploadService: FileUploadService) {
+  }
 
   async catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    console.log("AllExceptionFilter catch");
+    console.log('AllExceptionFilter catch');
 
     const status =
       exception instanceof HttpException
@@ -53,14 +54,14 @@ export class AllExceptionFilter implements ExceptionFilter {
     // Kiểm tra xem có file trong request không và xóa chúng nếu có
     const files = request['files'] || (request['file'] ? [request['file']] : null); // Cả trường hợp file và files
 
-    if (files) {
-      for (const file of files) {
-        try {
+    try {
+      if (files) {
+        for (const file of files) {
           await this.fileUploadService.deleteFile(file.path); // Xóa từng file
-        } catch (deleteError) {
-          console.error('Error deleting uploaded file:', deleteError.message);
         }
       }
+    } catch (deleteError) {
+      console.error('Error deleting uploaded file:', deleteError.message);
     }
 
     response.status(status).json({
